d54bc4696e9d04cd0fb65e406abee21302fb2c84
michael <michael.monticolo@gmail.com>
2013-12-22 15:40:43 +0100

    more priest changes

--- _LazyPigMultibox/LazyPigMultibox_Priest.lua
+++ _LazyPigMultibox/LazyPigMultibox_Priest.lua
@@ -17,20 +17,23 @@ function LazyPigMultibox_Priest(dps, dps_pet, heal, rez, buff)
 		QuickHeal()
 	end
 	
-	if buff then
-		if not shadow_form and Zorlen_castSpellByName("Shadowform") then
-			return
-		elseif castInnerFire() then
-			return
-		end	
-	end
+	
+	if not shadow_form and Zorlen_castSpellByName("Shadowform") then
+		return
+	elseif castInnerFire() then
+		return
+		
+	elseif (Zorlen_HealthPercent("player") < 50 or Zorlen_HealthPercent("player") < 75 and Zorlen_isEnemyTargetingYou("target")) and castPowerWordShield() then
+		return
+	end	
+
 	
 	if dps then
 		local plague_stack = Zorlen_GetDebuffStack("Spell_Shadow_BlackPlague", "target")
 		local fly_range = LazyPigMultibox_IsSpellInRangeAndActionBar("Mind Flay")
 
 		if isShootActive() and (not Zorlen_IsTimer("ShadowRotation") or Zorlen_ManaPercent("player") > 25 or plague_stack < 5) then
-			DEFAULT_CHAT_FRAME:AddMessage("Shoot Stop")
+			--DEFAULT_CHAT_FRAME:AddMessage("Shoot Stop")
 			stopShoot()
 			return
 		end	
@@ -38,34 +41,34 @@ function LazyPigMultibox_Priest(dps, dps_pet, heal, rez, buff)
 		if not Zorlen_IsTimer("ShadowWordPain") and Zorlen_ManaPercent("player") > 20 and castShadowWordPain() then
 			Zorlen_SetTimer(1, "ShadowWordPain")
 			Zorlen_SetTimer(9, "ShadowRotation")
-			DEFAULT_CHAT_FRAME:AddMessage("1")
+			---DEFAULT_CHAT_FRAME:AddMessage("1")
 			return
 		elseif Zorlen_ManaPercent("player") > 40 and isShadowWordPain() and castMindBlast() then
 			Zorlen_SetTimer(9, "ShadowRotation")
-			DEFAULT_CHAT_FRAME:AddMessage("2")
+			---DEFAULT_CHAT_FRAME:AddMessage("2")
 			return
 		
 		elseif not Zorlen_IsTimer("ShadowWordPain") and (not Zorlen_IsTimer("ShadowRotation") or plague_stack < 5) and castShadowWordPain(1) then
 			Zorlen_SetTimer(1, "ShadowWordPain")
 			Zorlen_SetTimer(9, "ShadowRotation")
-			DEFAULT_CHAT_FRAME:AddMessage("3")
+			---DEFAULT_CHAT_FRAME:AddMessage("3")
 			return
 		
 		elseif fly_range and Zorlen_ManaPercent("player") > 20 and castMindFlay() then 
-			DEFAULT_CHAT_FRAME:AddMessage("4")
 			Zorlen_SetTimer(9, "ShadowRotation")
+			--DEFAULT_CHAT_FRAME:AddMessage("4")
 			return
 		
 		elseif fly_range and (not Zorlen_IsTimer("ShadowRotation") or plague_stack < 5) and castMindFlay(1) then
 			Zorlen_SetTimer(9, "ShadowRotation")
-			DEFAULT_CHAT_FRAME:AddMessage("5")
+			--DEFAULT_CHAT_FRAME:AddMessage("5")
 			return
 		
 		elseif not shadow_form and castSmite() then
 			return
 			
 		elseif Zorlen_IsTimer("ShadowRotation") and plague_stack == 5 then
-			DEFAULT_CHAT_FRAME:AddMessage("Shoot Start")
+			--DEFAULT_CHAT_FRAME:AddMessage("Shoot Start")
 			castShoot()
 		end
 	end
